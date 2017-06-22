c0f937f9a120614c060d434cb3105389f28609ce
Joeffison Andrade <joeffison@gmail.com>
2017-06-22 18:16:44 -0300

    Adding ads by Google AdMob Beta

--- app/src/main/java/com/joeffison/jardimbotanico2/MainActivity.java
+++ app/src/main/java/com/joeffison/jardimbotanico2/MainActivity.java
@@ -1,6 +1,5 @@
 package com.joeffison.jardimbotanico2;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -20,16 +19,25 @@ import android.view.MenuItem;
 import android.webkit.WebView;
 //import android.widget.ShareActionProvider;
 
-import com.joeffison.jardimbotanico2.service.UtilityService;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 import com.joeffison.jardimbotanico2.util.MyWebViewHandler;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private AdView mAdView;
+    private AdView mAdViewBanner2;
+    private InterstitialAd mAdViewInterstitial;
 
     private MyWebViewHandler webViewHandler;
-    private ShareActionProvider mShareActionProvider;
-    private ShareActionProvider mShareActionProviderFB;
 
+    private ShareActionProvider mShareActionProvider;
+
+//    private ShareActionProvider mShareActionProviderFB;
+    //    private static final String ADMOB_APP_ID = "ca-app-pub-5566653721914999~9056708863";
     private static final String URL_APP_SITE_HOME = "https://joeffison.github.io/jb2/";
     private static final String URL_ABOUT_ME = "https://www.instagram.com/joeffison/";
     private static final String URL_ABOUT_ME_PROFESSIONAL = "https://github.com/Joeffison";
@@ -38,6 +46,8 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startAdService();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -61,6 +71,145 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         setUpWebView();
+    }
+
+    private void startAdService() {
+        MobileAds.initialize(this, getString(R.string.admob_app_id));
+        this.mAdView = (AdView) findViewById(R.id.adView);
+//        this.mAdViewBanner2 = (AdView) findViewById(R.id.adViewBanner2);
+//
+        setUpAd(this.mAdView);
+//        setUpAd(this.mAdViewBanner2);
+
+        // Interstitial ads occupies full screen of the app.
+        // Adding interstitial ad doesn’t require an AdView element to be added in the xml layout.
+        // Rather we load the ad programatically from the activity.
+        this.mAdViewInterstitial = new InterstitialAd(this);
+        // set the ad unit ID
+        mAdViewInterstitial.setAdUnitId(getString(R.string.admob_ad_unit_id_interstitial));
+
+        startInterstitialRequest();
+    }
+
+    private void startInterstitialRequest() {
+        if (this.mAdViewInterstitial != null) {
+            AdRequest adRequest = new AdRequest.Builder().build();
+            // Load ads into Interstitial Ads
+            mAdViewInterstitial.loadAd(adRequest);
+
+            mAdViewInterstitial.setAdListener(new AdListener() {
+                @Override
+                public void onAdLoaded() {
+                    // Code to be executed when an ad finishes loading.
+//                    Snackbar.make(MainActivity.this.mAdView, "onAdLoaded", Snackbar.LENGTH_LONG)
+//                            .setAction("Ads", null).show();
+                    showInterstitial();
+                }
+
+                @Override
+                public void onAdFailedToLoad(int errorCode) {
+                    // Code to be executed when an ad request fails.
+                    Snackbar.make(MainActivity.this.mAdView, "onAdFailedToLoad: " + errorCode, Snackbar.LENGTH_LONG)
+                            .setAction("Ads", null).show();
+                }
+
+//                @Override
+//                public void onAdOpened() {
+//                    // Code to be executed when an ad opens an overlay that
+//                    // covers the screen.
+//                    Snackbar.make(MainActivity.this.mAdView, "onAdOpened", Snackbar.LENGTH_LONG)
+//                            .setAction("Ads", null).show();
+//                }
+//
+//                @Override
+//                public void onAdLeftApplication() {
+//                    // Code to be executed when the user has left the app.
+//                    Snackbar.make(MainActivity.this.mAdView, "onAdLeftApplication", Snackbar.LENGTH_LONG)
+//                            .setAction("Ads", null).show();
+//                }
+//
+//                @Override
+//                public void onAdClosed() {
+//                    // Code to be executed when when the user is about to return
+//                    // to the app after tapping on an ad.
+//                    Snackbar.make(MainActivity.this.mAdView, "onAdClosed", Snackbar.LENGTH_LONG)
+//                            .setAction("Ads", null).show();
+//                }
+            });
+        }
+    }
+
+    private void setUpAd(final AdView adView) {
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.setAdListener(new AdListener(){
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                Snackbar.make(adView, "onAdLoaded", Snackbar.LENGTH_LONG)
+                        .setAction("Ads", null).show();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                Snackbar.make(adView, "onAdFailedToLoad: " + errorCode, Snackbar.LENGTH_LONG)
+                        .setAction("Ads", null).show();
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+                Snackbar.make(adView, "onAdOpened", Snackbar.LENGTH_LONG)
+                        .setAction("Ads", null).show();
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+                Snackbar.make(adView, "onAdLeftApplication", Snackbar.LENGTH_LONG)
+                        .setAction("Ads", null).show();
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the user is about to return
+                // to the app after tapping on an ad.
+                Snackbar.make(adView, "onAdClosed", Snackbar.LENGTH_LONG)
+                        .setAction("Ads", null).show();
+            }
+        });
+        adView.loadAd(adRequest);
+    }
+
+    private void showInterstitial() {
+        if (mAdViewInterstitial.isLoaded()) {
+            mAdViewInterstitial.show();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
     }
 
     private void setUpWebView() {
@@ -126,12 +275,15 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_home) {
 //            new UtilityService(this).list();
             this.webViewHandler.goHome();
+            startInterstitialRequest();
 //            Intent intent = new Intent(this, AdsActivity.class);
 //            startActivity(intent);
         } else if (id == R.id.nav_aboutme) {
             this.webViewHandler.goToUrl(URL_ABOUT_ME);
+            startInterstitialRequest();
         } else if (id == R.id.nav_aboutme_professional) {
             this.webViewHandler.goToUrl(URL_ABOUT_ME_PROFESSIONAL);
+            startInterstitialRequest();
 //        } else if (id == R.id.nav_manage) {
 //
         } else if (id == R.id.nav_share) {
